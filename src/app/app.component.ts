2f44fd56374e3773bd9ce8165a26ccad9449a8a0
Melissadrrs <melissadrrs@gmail.com>
2020-02-15 11:32:53 -0500

    probando1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import {FormBuilder,FormGroup,FormControl,Validators,FormArray} from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent implements OnInit {
+grupo: FormGroup;
+
+  constructor(private fb:FormBuilder){}
+
+  ngOnInit(){
+      this.grupo=this.fb.group({
+        nombre:[null,Validators.required]
+      }
+    )
+  }
 }
