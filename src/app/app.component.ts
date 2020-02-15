66c4f7e568bdf8416dbcf6e13fb3ed30c5133abf
Melissadrrs <melissadrrs@gmail.com>
2020-02-15 18:16:20 -0500

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,20 @@ import {FormBuilder,FormGroup,FormControl,Validators,FormArray} from '@angular/f
 })
 export class AppComponent implements OnInit {
 grupo: FormGroup;
-
+ArraySexo: Array<string> =['Mujer','Hombre'];
   constructor(private fb:FormBuilder){}
 
   ngOnInit(){
       this.grupo=this.fb.group({
-        nombre:[null,Validators.required]
-      }
-    )
+        nombre:[null,Validators.required],
+        apellido:[null,Validators.required],
+        correo:[null,[Validators.required,Validators.pattern(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/)]],
+        sexo:[null,Validators.required]
+        })
+
+  }
+
+  CallChange(event:any){
+    console.log(event.target)
   }
 }
