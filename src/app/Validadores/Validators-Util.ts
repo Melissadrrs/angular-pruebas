e44487000480e413e0ce5fddc75349bd6298d4b7
Melissadrrs <melissadrrs@gmail.com>
2020-02-15 21:52:42 -0500

    3 files modified

--- src/app/Validadores/Validators-Util.ts
+++ src/app/Validadores/Validators-Util.ts
@@ -6,4 +6,24 @@ import {
 } from '@angular/forms';
 
 export class ValidatorsUtil {
+
+  validadorCorreos(arr: string[]) {
+      return(control: FormControl):{ [key: string]: any }=>{
+if(!control || !control.value || control.value=='')
+return null;
+
+const aux=control.value.split('@');
+
+if(aux.length<2) return null;
+
+   if (arr.indexOf(aux[1].toLowerCase()) > -1) 
+        return { correoGratuito: true };
+      
+
+
+        return null;
+      };
+  
+  }
+
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit  } from '@angular/core';
 import {FormBuilder,FormGroup,FormControl,Validators,FormArray} from '@angular/forms';
 
+import {ValidatorsUtil} from './Validadores/Validators-Util';
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -9,13 +11,22 @@ import {FormBuilder,FormGroup,FormControl,Validators,FormArray} from '@angular/f
 export class AppComponent implements OnInit {
 grupo: FormGroup;
 ArraySexo: Array<string> =['Mujer','Hombre'];
+
+  dominiosCorreosGratuitos = [
+    'gmail.com',
+    'outlook.com',
+    'hotmail.com',
+    'yahoo.com',
+  ];
+ validatorsUtil: ValidatorsUtil = new ValidatorsUtil();
+
   constructor(private fb:FormBuilder){}
 
   ngOnInit(){
       this.grupo=this.fb.group({
         nombre:[null,Validators.required],
         apellido:[null,Validators.required],
-        correo:[null,[Validators.required,Validators.pattern(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/)]],
+        correo:[null,[Validators.required,Validators.pattern(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/),this.validatorsUtil.validadorCorreos(this.dominiosCorreosGratuitos)]],
         sexo:[null,Validators.required]
         })
 
